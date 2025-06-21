6d3fe08fd84dd7034543959f153b8448f6f04d2f
ramsey900 <ramsey.anyona@student.moringaschool.com>
2025-06-21 16:37:30 +0300

    the dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your code here!
+const element = document.createElement("div")
+document.body.appendChild(element);
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 100; i++) {
+  const li = document.createElement("li");
+  li.textContent = `Item ${i}`;
+  ul.appendChild(li);
+}       
+element.appendChild(ul);
+
+const main = document.getElementById("main");
+if (main) {
+  main.remove();
+}
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Ramsey Anyona is the champion";
+
+document.body.appendChild(newHeader);
